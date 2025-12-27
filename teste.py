fd6591dbefc0d2acee127def0b991f931f812621
jornada de dados <chsbene65@gmail.com>
2025-12-27 20:10:21 -0300

    mais uma alteraçao

--- teste.py
+++ teste.py
@@ -1,2 +1,6 @@
 nome = input('qual é o seu nome? ')
 print('prazer em te conhecer {:>20}!'.format(nome))
+
+n1 = int(input('um valor: '))
+n2 = int(input('outro valor: '))
+print('a soma vale {}'.format(n1+n2))
