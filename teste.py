2da1015b16eb45e6889d6a42bdebf9f1689201a5
jornada de dados <chsbene65@gmail.com>
2025-12-27 20:24:59 -0300

    mais uma alteraçao

--- teste.py
+++ teste.py
@@ -1,6 +1,15 @@
+import math as m
+
+
 nome = input('qual é o seu nome? ')
 print('prazer em te conhecer {:>20}!'.format(nome))
 
 n1 = int(input('um valor: '))
 n2 = int(input('outro valor: '))
-print('a soma vale {}'.format(n1+n2))
+s = n1 + n2 
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('a soma é {}, o produto é {} e a divisao é {:.3f}'.format(s, m, d), end=' ')
+print('divisao inteira {} e potencia {}'.format(di, e))
